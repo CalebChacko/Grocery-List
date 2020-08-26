606bb9520d9e739dec466b118715a67ebbb83ff5
Caleb Chacko <ctchacko@uwaterloo.ca>
2020-08-26 09:17:19 -0500

    Add cli structure to main()

--- grocerylist.py
+++ grocerylist.py
@@ -1,12 +1,45 @@
 import click
+from recipe import Recipe
+from recipebook import RecipeBook
+
+cookBook = RecipeBook()
 
 @click.command()
-@click.option("--count", default=1, help="Number of greetings.")
-@click.option("--name", prompt="Your name", help="The person to greet.")
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for _ in range(count):
-        click.echo(f"Hello, {name}!")
+@click.option("--target", prompt=True, help="Add to target location")
+@click.option("--action", prompt=True, help="Add to target location")
+def main(target, action):
+    cookBook.name = "This is mine"
+    if target == 'recipe':
+        if action == "add":
+            name = click.prompt("What's the recipe name")
+            cookBook.addRecipe(name)
+        elif action == "delete":
+            name = click.prompt("What's the recipe name")
+            recipeExists = cookBook.searchRecipe(name)
+            if recipeExists:
+                cookBook.deleteRecipe(name)
+            else:
+                print("Could not find recipe")
+        elif action == "update":
+            print("Update")
+        elif action == "search":
+            name = click.prompt("What's the recipe name")
+            recipeExists = cookBook.searchRecipe(name)
+            if recipeExists:
+                cookBook.deleteRecipe(name)
+            else:
+                print("Could not find recipe")
+        else:
+            print("Invalid action")
+    elif target == 'pantry':
+        print("I'm a pantry")
+    elif target == 'exit':
+        # Terminate program
+        return target
+
+    main()
+
 
 if __name__ == '__main__':
-    hello()
+
+    main()
